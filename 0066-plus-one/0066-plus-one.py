4969ecbbdb766c455ff2e434ab8d40c9a8851d16
suchibratarout <97304537+suchibratarout@users.noreply.github.com>
2023-05-23 05:08:18 +0530

    Time: 49 ms (24.41%), Space: 16.3 MB (7.01%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+         digits=digits[::-1]
+         x=1
+         ans=[]
+         for i in range(len(digits)):
+            ans+=[(digits[i]+x)%10]
+            x=(digits[i]+x)//10
+         if x:
+            ans+=[x]
+         return ans[::-1]
+        
