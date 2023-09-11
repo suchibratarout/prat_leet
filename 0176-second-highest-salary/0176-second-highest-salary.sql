217a744b760d76b76514c326812615b5df6db509
suchibratarout <97304537+suchibratarout@users.noreply.github.com>
2023-09-11 12:29:22 +0530

    Time: 911 ms (33.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,10 @@
-# Write your MySQL query statement below
-SELECT (
-    SELECT DISTINCT Salary
-      FROM Employee
-     ORDER BY Salary DESC
-     LIMIT 1
-    OFFSET 1
-) AS SecondHighestSalary;
+/* Write your PL/SQL query statement below */
+SELECT
+MAX(salary) as SecondHighestSalary
+FROM employee
+where salary < (
+SELECT
+MAX(salary)
+FROM
+employee);
+
